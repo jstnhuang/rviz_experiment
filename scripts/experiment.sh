3dc2e0378444b78a2afb8c3e0e354ca6e39df151
Justin Huang <jstn@cs.washington.edu>
2014-05-16 17:17:44 -0700

    Added check in script to prevent overwriting data.

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -1,1 +1,8 @@
-roslaunch rviz_experiment experiment.launch user:=$1 condition:=$2
+DATA_DIR=/home/jstn/Dropbox/experiment_data
+
+if [ -e $DATA_DIR/$1_$2.bag ]
+then
+  echo "$DATA_DIR/$1_$2.bag already exists! Stopping launch."
+else
+  roslaunch rviz_experiment experiment.launch user:=$1 condition:=$2
+fi
